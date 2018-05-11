3a8ac72f860f4ecba19c491eaf7430359aa4fd3a
Bas <basnetten@gmail.com>
2018-05-11 16:19:11 +0200

    Add DisjointSet with tests
    Add the disjoint set data structure and tests. This data structure can be
    used for uniting indices.

---
+++ DataStructures/DisjointSet.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace DataStructures
+{
+	/// <summary>
+	/// DisjointSet is a data structure that can be used to unite sets of indices.
+	/// </summary>
+	public class DisjointSet
+	{	
+		/// <summary>
+		/// The value that every index in the array is going to have initially. This is usually set to -1 for Union by
+		/// size.
+		/// </summary>
+		private const int InitialValue = -1;
+
+		/// <summary>
+		/// Initialize a disjoint set with a certain size. It will be filled with the <see cref="InitialValue"/> at
+		/// every index.
+		/// </summary>
+		/// <param name="size">The size that the array of the disjoint set will be.</param>
+		public DisjointSet(int size)
+		{
+			Array = new int[size];
+
+			for (int i = 0; i < size; i++)
+				Array[i] = InitialValue;
+		}
+
+		/// <summary>
+		/// Initialize a disjoint set with an array. The array will not be copied. The values will also not be checked,
+		/// so use with care. This is mainly intended for testing.
+		/// </summary>
+		/// <param name="array">The array reference to use.</param>
+		public DisjointSet(int[] array)
+		{
+			Array = array;
+		}
+
+		/// <summary>
+		/// Indexer to access the underlying array of the disjoint set. This is mainly intended for testing, but should
+		/// function fine in any situation.
+		/// </summary>
+		public int this[int index] => Array[index];
+
+		/// <summary>
+		/// The actual array with the associated values.
+		/// </summary>
+		private int[] Array { get; }
+
+		/// <summary>
+		/// Find the root index of the given index. If the given index is a root, it will simply be returned.
+		/// </summary>
+		/// <remarks>
+		///	This Find function implements path compression. This should improve performance.
+		/// </remarks>
+		/// <param name="i">Index to find the root of.</param>
+		/// <returns>Index of the root of <paramref name="i"/>.</returns>
+		public int Find(int i)
+		{
+			// If < 0 this is a root, therefore return i.
+			if (Array[i] < 0) return i;
+			// Else find the root of the index that is the root of i. Set the result in the array for path compression.
+			return Array[i] = Find(Array[i]);
+		}
+
+		/// <summary>
+		/// Execute a union on two indices.
+		/// </summary>
+		/// <remarks>
+		/// The indices do not have to be roots, <see cref="Find"/> will be called on both. If the root is the same for
+		/// both indices, nothing happens. Uses Union by Size to determine which index will be the root.
+		/// </remarks>
+		public void Union(int i, int j)
+		{
+			int rootI = Find(i);
+			int rootJ = Find(j);
+
+			// J has the bigger tree, so we flip the roots.
+			if (Array[rootI] > Array[rootJ])
+			{
+				int tmp = rootI;
+				rootI = rootJ;
+				rootJ = tmp;
+			}
+
+			// Increase the size of rootI by the size of rootJ. Then set the root of rootJ to rootI.
+			Array[rootI] += Array[rootJ];
+			Array[rootJ] = rootI;
+		}
+
+		/**
+		 * A simple ToString for debugging.
+		 */
+		public override string ToString()
+		{
+			// First line, indices.
+			string str = String.Empty;
+			for (int i = 0; i < Array.Length; i++)
+			{
+				if (i != 0) str += ", ";
+				str += $"{i:0000}";
+			}
+
+			str += Environment.NewLine;
+
+			// Second line, values.
+			for (int i = 0; i < Array.Length; i++)
+			{
+				if (i != 0) str += ", ";
+				if (Array[i] < 0)
+					str += $"{Array[i]:000}";
+				else
+					str += $"{Array[i]:0000}";
+			}
+
+			return str;
+		}
+	}
+}
---
+++ DataStructuresTests/DisjointSetTests.cs
@@ -0,0 +1,70 @@
+﻿using DataStructures;
+using Xunit;
+
+namespace DataStructuresTests
+{
+	public class DisjointSetTests
+	{
+		[Theory]
+		[InlineData(10, 0)]
+		[InlineData(10, 5)]
+		[InlineData(10, 9)]
+		[InlineData(1000, 999)]
+		[InlineData(1000, 0)]
+		[InlineData(1000, 42)]
+		public void Find_Should_FindSelf_When_SetEmpty(int size, int i)
+		{
+			DisjointSet disjointSet = new DisjointSet(size);
+			int actual = disjointSet.Find(i);
+			Assert.Equal(i, actual);
+		}
+
+		[Theory]
+		[InlineData(new[] {4, 5, 6}, 0)]
+		[InlineData(new[] {4, 5, 6}, 1)]
+		[InlineData(new[] {4, 5, 6}, 2)]
+		public void Indexer_Should_ReturnValueOfArray_When_IndexValid(int[] array, int index)
+		{
+			DisjointSet disjointSet = new DisjointSet(array);
+			int actual = disjointSet[index];
+			int expected = array[index];
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData(10, 0, 1)]
+		[InlineData(10, 1, 0)]
+		[InlineData(10, 0, 9)]
+		[InlineData(10, 9, 0)]
+		[InlineData(10, 5, 9)]
+		[InlineData(1000, 0, 999)]
+		[InlineData(1000, 999, 0)]
+		public void Union_Should_UniteRoots_When_SetEmpty(int size, int i1, int i2)
+		{
+			DisjointSet disjointSet = new DisjointSet(size);
+			disjointSet.Union(i1, i2);
+
+			int actualDepthI1 = disjointSet[i1];
+			int actualParentI2 = disjointSet[i2];
+
+			Assert.Equal(-2, actualDepthI1);
+			Assert.Equal(i1, actualParentI2);
+		}
+
+		[Theory]
+		[InlineData(new[] {-1, -1, -1, -1}, new[] {-1, -1, -1, -1}, 1)]
+		[InlineData(new[] {-3, 0, 1, -1}, new[] {-3, 0, 0, -1}, 2)]
+		[InlineData(new[] {3, 0, 1, -4}, new[] {3, 3, 1, -4}, 1)]
+		[InlineData(new[] {3, 0, 1, -4}, new[] {3, 3, 3, -4}, 2)]
+		public void Find_ShouldCompressPath_When_Possible(int[] start, int[] expected, int find)
+		{
+			DisjointSet disjointSet = new DisjointSet(start);
+			disjointSet.Find(find);
+
+			for (int i = 0; i < start.Length; i++)
+			{
+				Assert.Equal(expected[i], disjointSet[i]);
+			}
+		}
+	}
+}
