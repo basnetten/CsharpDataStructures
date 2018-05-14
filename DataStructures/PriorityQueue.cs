cb7bf8bc8731997422a9319bc7ba0b23d666b436
Bas <basnetten@gmail.com>
2018-05-14 23:04:58 +0200

    Refactor PriorityQueue
    Fix an issue with the Insert Function. The CapacityDepth was not updated properly. This caused the Store to be bigger than needed.
    
    Fix an issue with the PercolateUp Function. The parent was compared to the child, rather than the data stored at index 0.
    
    Rename some properties and functions to better reflect their usage. Namely these are the Depth, now named CapacityDepth as the depth the the depth of the capacity, not the data, and the DeleteMin function, now named Dequeue. DeleteMin is renamed to have the PriorityQueue look more like the built-in Queue. Add several comments as well as some extra unit tests.

--- DataStructures/PriorityQueue.cs
+++ DataStructures/PriorityQueue.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Security.Policy;
 
 namespace DataStructures
 {
@@ -22,21 +17,21 @@ namespace DataStructures
 			Store = new T[ArraySizeFromDepth(InitialDepth)];
 
 			Count = 0;
-			Depth = InitialDepth;
+			CapacityDepth = InitialDepth;
 		}
 
-		/// <summary>
-		/// The current depth of the queue.
-		/// </summary>
-		public int Depth { get; private set; }
-		
 		/// <summary>
 		/// The current number of elements in the queue.
 		/// </summary>
 		public int Count { get; private set; }
+		
+		/// <summary>
+		/// The current depth of the queue. The depth is directly related to <see cref="Capacity"/>.
+		/// </summary>
+		public int CapacityDepth { get; private set; }
 
 		/// <summary>
-		/// The current capacity of the queue.
+		/// The current capacity of the queue. The capacity is directly related to <see cref="CapacityDepth"/>.
 		/// </summary>
 		public int Capacity => Store.Length - 1;
 
@@ -46,85 +41,115 @@ namespace DataStructures
 		private T[] Store { get; set; }
 
 		/// <summary>
+		/// Insert a new value into the queue.
+		/// </summary>
+		/// <param name="data">The data to be inserted into the queue.</param>
+		public void Insert(T data)
+		{
+			// Increase the Count of elements, and resize if necessary.
+			Count += 1;
+			if (Count > Capacity) Resize(++CapacityDepth);
+			
+			// Place the data in the 0th element. This is in preparation for the PercolateUp algorithm.
+			Store[0] = data;
+			
+			// Move the data into the correct position.
+			PercolateUp(Count);
+		}
+
+		/// <summary>
+		/// Retrieve the first item in the queue.
+		/// </summary>
+		/// <returns>Minimal value in the queue.</returns>
+		public T Peek()
+		{
+			return Store[1];
+		}
+		
+		/// <summary>
 		/// Retrieve and delete the first item in the queue.
 		/// </summary>
 		/// <returns>Minimal value in the queue.</returns>
-		public T DeleteMin()
+		public T Dequeue()
 		{
 			// Retrieve minimal value.
 			T result = Store[1];
-			
+
 			// Move the last item in the queue to the start.
 			Store[1] = Store[Count];
 			Count--;
-			
+
 			// Percolate the ex-last item in the queue down.
 			PercolateDown(1);
 
 			return result;
 		}
 
-		public void Insert(T data)
-		{
-			Count += 1;
-			if (Count >= Capacity) Resize(Depth++);
-			Store[0] = data;
-			PercolateUp(Count);
-		}
-
+		/// <summary>
+		/// Build the heap from a random set of data.
+		/// </summary>
+		/// <remarks>
+		/// The <see cref="Capacity"/> and <see cref="CapacityDepth"/> are changed to the minimal possible to still
+		/// fit the data array.
+		/// </remarks>
+		/// <param name="data">Array with data to be inserted into the heap.</param>
 		public void BuildHeap(T[] data)
 		{
+			// First the count is set.
 			Count = data.Length;
-			Depth = DepthFromArraySize(data.Length + 1);
-			Resize(Depth);
+			
+			// Then the smallest depth that can fit the data is determined.
+			CapacityDepth = DepthFromArraySize(data.Length + 1);
+			
+			// The depth is then used to resize the store, and the data copied to it.
+			Resize(CapacityDepth);
 			data.CopyTo(Store, 1);
 
+			// Finally every index is percolated down to its correct position in the queue.
 			for (int i = Capacity / 2; i > 0; i--)
 				PercolateDown(i);
 		}
 
+		/// <summary>
+		/// Percolates the index down (towards higher indices) to where its parent is bigger than it, and its
+		/// children smaller.
+		/// </summary>
 		private void PercolateDown(int index)
 		{
+			// The data that needs to percolate down.
 			T data = Store[index];
 
+			// While still in range of the store, loop through the children.
 			for (int childIndex = 0; index * 2 <= Count; index = childIndex)
 			{
 				childIndex = index * 2;
+				
+				// If the childindex is less than count, there is still a child in the right node too, so check
+				// which of the children has the smaller value. If the right child is smaller, the child index
+				// is increased to point to this child.
 				if (childIndex < Count && Store[childIndex].CompareTo(Store[childIndex + 1]) > 0)
 					childIndex++;
 
+				// Check if the smallest child is smaller than the data, if so, move the child up and continue
+				// the loop, otherwise break.
 				if (data.CompareTo(Store[childIndex]) > 0)
 					Store[index] = Store[childIndex];
 				else
 					break;
 			}
+			// Note that when the end of the store (Count) is reached, the index will be in the last position
+			// and the data will be inserted there.
 
+			// Finally assign the data to the correct position, indicated by the index state.
 			Store[index] = data;
 		}
 
 		private void PercolateUp(int index)
 		{
-			for (; Store[index].CompareTo(Store[index / 2]) < 0; index /= 2)
+			for (; Store[0].CompareTo(Store[index / 2]) < 0; index /= 2)
 				Store[index] = Store[index / 2];
 			Store[index] = Store[0];
 			Store[0] = default(T);
-		}
-
-		private void PercolateDown(T data)
-		{
-			int index = Count;
-			while (true)
-			{
-				if (index != 0 && data.CompareTo(Store[index / 2]) < 0)
-				{
-					Store[index] = Store[index / 2];
-					index = index / 2;
-					continue;
-				}
-
-				Store[index] = data;
-				break;
-			}
 		}
 
 		public override string ToString()
@@ -147,8 +172,14 @@ namespace DataStructures
 			Store = resizedArray;
 		}
 
+		/// <summary>
+		/// Determines the size that the store needs to be to support depth.
+		/// </summary>
 		private static int ArraySizeFromDepth(int depth) => (int) Math.Pow(2, depth);
 
+		/// <summary>
+		/// Determines the smallest depth required to fit a dataset of size.
+		/// </summary>
 		private static int DepthFromArraySize(int size) => (int) Math.Log(size, 2);
 	}
 }
--- DataStructuresTests/PriorityQueueTests.cs
+++ DataStructuresTests/PriorityQueueTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
 using DataStructures;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,33 +16,6 @@ namespace DataStructuresTests
 		}
 
 		[Theory]
-		[InlineData(new[] {92, 47, 21, 20, 12, 45, 63, 61, 17, 55, 37, 25, 64, 83, 73})]
-		public void BuildHeap_Should_BuildHeap_WhenCorrectArray(int[] values)
-		{
-			PriorityQueue<int> queue = new PriorityQueue<int>();
-
-			queue.BuildHeap(values);
-
-			var list = values.ToList();
-			list.Sort();
-			for (int i = 0; i < values.Length; i++)
-				Assert.Equal(list[i], queue.DeleteMin());
-		}
-
-		[Theory]
-		[InlineData(-5)]
-		[InlineData(0)]
-		[InlineData(5)]
-		public void Insert_Should_InsertValueCorrectly_When_Empty(int value)
-		{
-			PriorityQueue<int> queue = new PriorityQueue<int>();
-
-			queue.Insert(value);
-
-			Assert.Equal(value, queue.DeleteMin());
-		}
-
-		[Theory]
 		[InlineData(new[] {1, 2, 3, 4})]
 		[InlineData(new[] {1, 2, 3, 4, -1, 45})]
 		[InlineData(new[] {-5})]
@@ -57,6 +29,85 @@ namespace DataStructuresTests
 			}
 
 			Assert.Equal(values.Length, queue.Count);
+		}
+
+		[Theory]
+		[InlineData(3, 2)]
+		[InlineData(4, 3)]
+		[InlineData(7, 3)]
+		[InlineData(8, 4)]
+		[InlineData(15, 4)]
+		[InlineData(16, 5)]
+		public void Depth_Should_ReturnCorrectDepth_When_IncreasingCapacity(int count, int expectedDepth)
+		{
+			PriorityQueue<int> queue = new PriorityQueue<int>();
+			for (int i = 0; i < count; i++)
+				queue.Insert(i);
+
+			int actual = queue.CapacityDepth;
+
+			Assert.Equal(expectedDepth, actual);
+		}
+
+		[Theory]
+		[InlineData(3, 3)]
+		[InlineData(4, 7)]
+		[InlineData(7, 7)]
+		[InlineData(8, 15)]
+		[InlineData(15, 15)]
+		[InlineData(16, 31)]
+		public void Capacity_Should_ReturnCapacity_When_IncreasingCapacity(int count, int expectedCapacity)
+		{
+			PriorityQueue<int> queue = new PriorityQueue<int>();
+			for (int i = 0; i < count; i++)
+				queue.Insert(i);
+
+			int actual = queue.Capacity;
+
+			Assert.Equal(expectedCapacity, actual);
+		}
+
+		[Theory]
+		[InlineData(-5)]
+		[InlineData(0)]
+		[InlineData(5)]
+		public void Insert_Should_InsertValueCorrectly_When_Empty(int value)
+		{
+			PriorityQueue<int> queue = new PriorityQueue<int>();
+
+			queue.Insert(value);
+
+			Assert.Equal(value, queue.Dequeue());
+		}
+
+		[Theory]
+		[InlineData(new[] {1, 2, 3, 4})]
+		[InlineData(new[] {1, 2, 3, 4, -1, 45})]
+		[InlineData(new[] {-5})]
+		public void Peek_Should_ReturnMinvalue_When_Available(int[] values)
+		{
+			PriorityQueue<int> queue = new PriorityQueue<int>();
+			for (int i = 0; i < values.Length; i++)
+				queue.Insert(values[i]);
+			int expectedValue = values.Min();
+			
+			int actual = queue.Peek();
+
+			Assert.Equal(expectedValue, actual);
+		}
+
+		[Theory]
+		[InlineData(new[] {92, 47, 21, 20, 12, 45, 63, 61, 17, 55, 37, 25, 64, 83, 73})]
+		public void BuildHeap_Should_BuildHeap_WhenCorrectArray(int[] values)
+		{
+			PriorityQueue<int> queue = new PriorityQueue<int>();
+
+			queue.BuildHeap(values);
+
+			var list = values.ToList();
+			list.Sort();
+			for (int i = 0; i < values.Length; i++)
+				Assert.Equal(list[i], queue.Dequeue());
 		}
 	}
 }
