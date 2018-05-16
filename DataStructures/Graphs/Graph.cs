e794a6969946b95895a21995d175011cfe8fc3ad
Bas <basnetten@gmail.com>
2018-05-16 22:36:58 +0200

    Add Graph Data Structure
    Add a simple implementation of the Graph data structure. A simple test
    of the dijkstra algorithm is included in the DataStructuresTests project.

---
+++ DataStructures/Graphs/Graph.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Graphs
+{
+	/// <summary>
+	/// A simple graph class, usable for all kinds of purposes.
+	/// </summary>
+	/// <typeparam name="TNodeId">Identifier for the nodes.</typeparam>
+	public class Graph<TNodeId>
+	{
+		/// <summary>
+		/// Intitialize a new Graph with an empty list of nodes.
+		/// </summary>
+		public Graph()
+		{
+			Nodes = new Dictionary<TNodeId, Node>();
+		}
+
+		/// <summary>
+		/// The nodes that are in this Graph. They are indexed by <see cref="TNodeId"/>.
+		/// </summary>
+		public Dictionary<TNodeId, Node> Nodes { get; }
+
+		/// <summary>
+		/// Add an existing node to this Graph.
+		/// </summary>
+		public void AddNode(TNodeId identifier, Node node) => Nodes.Add(identifier, node);
+
+		/// <summary>
+		/// Add a new node to the Graph with identifier as index. Calls the default Node constructor.
+		/// </summary>
+		public void AddEmptyNode(TNodeId identifier) => Nodes.Add(identifier, new Node());
+
+		/// <summary>
+		/// Add a new edge to the Graph. Edges are stored in the individual Nodes.
+		/// </summary>
+		public void AddEdge(TNodeId source, TNodeId destination, double cost = 0d)
+			=> Nodes[source].AddEdge(new Edge(destination, cost));
+
+		public override string ToString()
+		{
+			return string.Join(Environment.NewLine, Nodes);
+		}
+
+		/**
+		 * Subclasses.
+		 */
+		/// <summary>
+		/// A simple Edge class to accompany the <see cref="Graph{TNodeId}"/> class.
+		/// </summary>
+		public class Edge
+		{
+			/// <summary>
+			/// Initialize a new Edge with destination and optionally a cost.
+			/// </summary>
+			public Edge(TNodeId destination, double cost = 0d)
+			{
+				Destination = destination;
+				Cost = cost;
+			}
+
+			/// <summary>
+			/// The destination index of this edge.
+			/// </summary>
+			public TNodeId Destination { get; }
+
+			/// <summary>
+			/// The cost to get to the destination over this edge.
+			/// </summary>
+			public double Cost { get; set; }
+
+			public override string ToString()
+			{
+				// ReSharper disable once CompareOfFloatsByEqualityOperator
+				return $"({Destination}{(Cost == 0 ? "" : $", {Cost}")})";
+			}
+		}
+
+		/// <summary>
+		/// A simple Node class to accompany the <see cref="Graph{TNodeId}"/> class.
+		/// </summary>
+		public class Node
+		{
+			/// <summary>
+			/// Initialize a Node with an empty list of Edges.
+			/// </summary>
+			public Node()
+			{
+				Edges = new List<Edge>();
+			}
+
+			/// <summary>
+			/// The edges that can be travelled from this node.
+			/// </summary>
+			public List<Edge> Edges { get; }
+
+			/// <summary>
+			/// Add an existing Edge to this node.
+			/// </summary>
+			public void AddEdge(Edge edge) => Edges.Add(edge);
+
+			public override string ToString()
+			{
+				return $"Node: [{string.Join(", ", Edges)}]";
+			}
+		}
+	}
+}
---
+++ DataStructuresTests/GraphTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataStructures;
+using DataStructures.Graphs;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DataStructuresTests
+{
+	public class GraphTests
+	{
+		private ITestOutputHelper _out;
+
+		public GraphTests(ITestOutputHelper @out)
+		{
+			_out = @out;
+		}
+
+		[Fact]
+		public void NodeAddEdge_Should_SaveEdge()
+		{
+			var graph = new Graph<int>();
+			// Add all the nodes.
+			graph.AddEmptyNode(0);
+			graph.AddEmptyNode(1);
+			graph.AddEmptyNode(2);
+			graph.AddEmptyNode(3);
+			graph.AddEmptyNode(4);
+			graph.AddEmptyNode(5);
+			graph.AddEmptyNode(6);
+
+			// Connect the nodes.
+			graph.AddEdge(0, 1, 2);
+			graph.AddEdge(0, 3, 1);
+			graph.AddEdge(1, 3, 3);
+			graph.AddEdge(1, 4, 10);
+			graph.AddEdge(2, 0, 4);
+			graph.AddEdge(2, 5, 5);
+			graph.AddEdge(3, 2, 2);
+			graph.AddEdge(3, 4, 2);
+			graph.AddEdge(3, 5, 8);
+			graph.AddEdge(3, 6, 4);
+			graph.AddEdge(4, 6, 6);
+			graph.AddEdge(6, 5, 1);
+
+			// State variables.
+			var queue = new PriorityQueue<Path>();
+			var processed = new HashSet<int>();
+			var bestPaths = new Dictionary<int, Path>();
+
+			// Insert the first node. The search will start here.
+			queue.Insert(new Path(new List<int> {0}, 0));
+
+			// Run until the queue is empty.
+			while (queue.Count > 0)
+			{
+				// The path being evaluated this loop.
+				Path pathUnderEval = queue.Dequeue();
+
+				// If the node that this path leads to has already been processed, it already has the best possible
+				// path stored. Therefore the algoritm can skip processing this node.
+				if (processed.Contains(pathUnderEval.LastOfRoute)) continue;
+				processed.Add(pathUnderEval.LastOfRoute);
+
+				// Retrieve the node from the Graph instance.
+				Graph<int>.Node node = graph.Nodes[pathUnderEval.LastOfRoute];
+				foreach (var edge in node.Edges)
+				{
+					// Calculate the length to reach the destination of this edge.
+					double edgeLength = pathUnderEval.Length + edge.Cost;
+
+					// If the destination of this edge is known, and the stored length is less (better) than the
+					// length of the edge being evaluated now, this edge can be skipped. If it is not known, or this
+					// path is better, the algorithm continues.
+					if (bestPaths.ContainsKey(edge.Destination) && bestPaths[edge.Destination].Length < edgeLength) continue;
+
+					// Copy the route of the path and add the destination of the edge to it..
+					var edgeRoute = new List<int>(pathUnderEval.Route);
+					edgeRoute.Add(edge.Destination);
+
+					// Initialize the better path, and store and queue it.
+					var betterPath = new Path(edgeRoute, edgeLength);
+					bestPaths[edge.Destination] = betterPath;
+					queue.Insert(betterPath);
+				}
+			}
+
+			_out.WriteLine($"{graph}");
+			_out.WriteLine($"{string.Join(Environment.NewLine, bestPaths)}");
+
+			Assert.True(true);
+		}
+
+		class Path : IComparable<Path>
+		{
+			public Path(List<int> route, double length)
+			{
+				var tmp = new int[route.Count];
+				route.CopyTo(tmp);
+				Route = tmp.ToList();
+
+				Length = length;
+			}
+
+			public List<int> Route { get; }
+			public int LastOfRoute => Route.Last();
+			public double Length { get; set; }
+
+			public int CompareTo(Path other)
+			{
+				if (Length < other.Length) return -1;
+				if (Length > other.Length) return 1;
+				return 0;
+			}
+
+			public override string ToString()
+			{
+				return $"{Length} : {string.Join(", ", Route)}";
+			}
+		}
+	}
+}
